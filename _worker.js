0926a8a4e8ebf92a107ed092c2fd5344cc0cf234
18807267207 <79787347+18807267207@users.noreply.github.com>
2026-04-11 22:00:29 +0800

    Update key in _worker.js
    Replace placeholder comment with actual key value.

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
 
-    // 这里填入你刚才发给我的那串完整的 Key
+    // 17b2dfa542140f462a2653d10b8ac998c6d916072ef2804343603f56ae323f67d73fc1a25395321e0e1d53fe30b67d3e345ff3daafcf2a8502280ccbe0fdaf7c Key
     const MY_KEY = "17b2dfa542140f462a2653d10b8ac998c6d916072ef2804343603f56ae323f67d73fc1a25395321e0e1d53fe30b67d3e345ff3daafcf2a8502280ccbe0fdaf7c";
 
     // 只要有人访问 validation-key.txt，就直接返回这个 Key
